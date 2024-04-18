2771a080beb424f2fdc3ccf3c3eb17f6cd782373
Dominique Broeglin <dominique.broeglin@gmail.com>
2024-04-18 22:50:56 +0200

    Copy Azure-Samples/azure-search-openai-demo

--- generators/app/index.js
+++ generators/app/index.js
@@ -2,37 +2,43 @@
 import Generator from "yeoman-generator";
 import chalk from "chalk";
 import yosay from "yosay";
+import tmp from "tmp";
 
 export default class extends Generator {
+  constructor(args, opts) {
+    super(args, opts);
+
+    this.argument("appname", { type: String, required: true });
+
+    // And you can then access it later; e.g.
+    this.log(this.options.appname);
+  }
+
   prompting() {
-    // Have Yeoman greet the user.
-    this.log(
-      yosay(
-        `Welcome to the groundbreaking ${chalk.red(
-          "generator-nifty-az"
-        )} generator!`
-      )
-    );
-
-    const prompts = [
-      {
-        type: "confirm",
-        name: "someAnswer",
-        message: "Would you like to enable this option?",
-        default: true
-      }
-    ];
-
-    return this.prompt(prompts).then(props => {
-      // To access props later use this.props.someAnswer;
-      this.props = props;
-    });
+    this.log(yosay(`Welcome ${chalk.red("Nifty AZ")} generator!`));
   }
 
   writing() {
-    this.fs.copy(
-      this.templatePath("dummyfile.txt"),
-      this.destinationPath("dummyfile.txt")
-    );
+    this.cloneDir = tmp.dirSync();
+
+    this.spawnSync("git", [
+      "clone",
+      "--depth=1",
+      "https://github.com/Azure-Samples/azure-search-openai-demo.git",
+      this.cloneDir.name
+    ]);
+    this.spawnSync("rm", ["-rf", `${this.cloneDir.name}/.git`]);
+
+    this.fs.copy(this.cloneDir.name, this.destinationRoot(), {
+      globOptions: { dot: true }
+    });
+  }
+
+  install() {
+    this.installDependencies({
+      npm: false,
+      bower: false,
+      yarn: false
+    });
   }
 }
