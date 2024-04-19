ea011fb86cd84b34ac14e2046faaab7ac389d81b
Dominique Broeglin <dominique.broeglin@gmail.com>
2024-04-20 00:44:07 +0200

    Emptying data & .git, git init after generation

--- generators/app/index.js
+++ generators/app/index.js
@@ -7,32 +7,35 @@ import tmp from "tmp";
 export default class extends Generator {
   constructor(args, opts) {
     super(args, opts);
-
     this.argument("destination", { type: String, required: true });
+    this.destinationRoot(this.options.destination);
 
     this.log("Generating in '" + this.options.destination + "'");
-    this.destinationRoot(this.options.destination);
   }
 
   prompting() {
-    this.log(yosay(`Welcome ${chalk.red("Nifty AZ")} generator!`));
+    this.log(yosay(`Welcome to ${chalk.red("Nifty AZ")} generator!`));
   }
 
   writing() {
     this.cloneDir = tmp.dirSync();
-
     this.spawnSync("git", [
       "clone",
       "--depth=1",
       "https://github.com/Azure-Samples/azure-search-openai-demo.git",
       this.cloneDir.name
     ]);
-    this.spawnSync("rm", ["-rf", `${this.cloneDir.name}/.git`]);
 
     this.fs.copy(this.cloneDir.name, this.destinationRoot(), {
-      globOptions: { dot: true }
+      globOptions: {
+        dot: true,
+        ignore: ["**/data", "**/.git"]
+      }
     });
   }
 
-  install() {}
+  install() {
+    this.spawnSync("mkdir", ["data"]);
+    this.spawnSync("git", ["init"]);
+  }
 }
